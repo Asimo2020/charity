790c3663f8e31327d8e150bd4826518de6a38afa
Asimo202 <ghahramani89@gmail.com>
2024-08-07 23:42:28 +0330

    create models

--- charities/models.py
+++ charities/models.py
@@ -1,13 +1,54 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.conf import settings  
 
 class Benefactor(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    EXPERIENCE_CHOICES = [  
+        (0, 'مبتدی'),  
+        (1, 'متوسط'),     
+        (2, 'متخصص'),     
+    ]   
+      
+    experience = models.SmallIntegerField(  
+        choices=EXPERIENCE_CHOICES,  
+        default=0  
+    )  
+    free_time_per_week = models.PositiveSmallIntegerField(  
+        default=0    
+    )  
 
+    def __str__(self):  
+        return f"{self.user.username} - {self.get_experience_display()}"  
 
 class Charity(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
+    def __str__(self):  
+        return f"{self.user.user}"  
 
 
 class Task(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    assigned_benefactor = models.ForeignKey(Benefactor, on_delete=models.CASCADE, null=True)
+    charity = models.ForeignKey(Charity, on_delete=models.CASCADE) 
+    age_limit_from = models.IntegerField(blank=True,null=True)
+    age_limit_to = models.IntegerField(blank=True,null=True)
+    date = models.DateField(blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+    gender_limit = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')],blank=True,null=True)
+    STATUS_CHOICES = [  
+        ('P', 'Pending'),  
+        ('W', 'Waiting'),  
+        ('A', 'Assigned'),  
+        ('D', 'Done'),  
+    ]  
+    state = models.CharField(max_length=1,choices=STATUS_CHOICES,default='P',)  
+    title = models.CharField(max_length= 60)
+    def __str__(self):  
+        return self.charity  
