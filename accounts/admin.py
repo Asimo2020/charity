5212137914fca462df702aeae91e839fc6b30e91
Asimo202 <ghahramani89@gmail.com>
2024-08-09 10:28:20 +0330

    set admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):  
+    ordering = ['username']  
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',]  
+    search_fields = ('username', 'email', 'first_name', 'last_name') 
+admin.site.register(User, CustomUserAdmin) 
