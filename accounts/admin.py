6ef94f628599817cf7f812237c3bfa34b88c5df6
Asimo202 <ghahramani89@gmail.com>
2024-08-09 15:57:24 +0330

    edit and sort headers of user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,49 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-class CustomUserAdmin(admin.ModelAdmin):  
+
+class ChangeUserAdmin(admin.ModelAdmin):  
+    search_fields = ('username', 'email', 'first_name', 'last_name') 
+    list_filter = ('gender', 'is_staff', 'is_superuser', 'is_active') 
     ordering = ['username']  
     list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',]  
-    search_fields = ('username', 'email', 'first_name', 'last_name') 
-admin.site.register(User, CustomUserAdmin) 
+    fieldsets = (  
+        (None,  
+         {  
+            'fields': (  
+                'username',   
+                'password',  
+            ),
+         }),  
+        ('Personal info',   
+         {  
+            'fields': (  
+                'first_name',   
+                'last_name',  
+                'email',  
+                'gender',  
+                'age',       
+                'description'   
+            ),  
+        }),  
+        ('Contact info',   
+         {  
+            'fields': (  
+                'phone',   
+                'address',  
+            ),   
+        }),  
+        ('Permissions', {  
+            "fields": (  
+                "is_active",  
+                "is_staff",  
+                "is_superuser",  
+                "groups",  
+                "user_permissions",  
+            ),  
+        }),  
+        ('Important dates', {"fields": ("last_login", "date_joined")}),  
+    )  
+
+admin.site.register(User, ChangeUserAdmin)
+
